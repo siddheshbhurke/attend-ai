6091e99554d15817fc4e5ef8e8c41a9a236fb5c0
Siddhesh <siddheshbhurke28@gmail.com>
2026-07-05 21:55:05 +0530

    cards updated

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -2,90 +2,93 @@ import streamlit as st
 
 def style_background_home():
     st.markdown("""
-    <style>
+        <style>
                 .stApp{
-                    background: #5865f2 !important
+                    background:#5865F2 !important
                 }
                 .stApp div[data-testid="stColumn"]{
-                background-color: #e0e3ff !important;
-                padding:2.5rem !important;
-                border-radius: 5rem !important;}
-    </style>            
+                    background-color:#E0E3FF !important;
+                    padding:2.5rem !important;
+                    border-radius:5rem !important
+                }
+        </style>
 
-""", unsafe_allow_html=True)
+                """
+            ,unsafe_allow_html=True)
     
 def style_background_dashboard():
     st.markdown("""
-    <style>
+        <style>
                 .stApp{
-                    background: #e0e3ff !important
+                    background:#E0E3FF !important
                 }
-    </style>            
+                
+        </style>
 
-""", unsafe_allow_html=True)
+                """
+            ,unsafe_allow_html=True)
     
 def style_base_layout():
     st.markdown("""
-    <style>
-    @import url('https://fonts.googleapis.com/css2?family=Climate+Crisis:YEAR@1979&display=swap'); 
-    @import url('https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap');             
-     #MainMenu, header,footer{
-      visibility:hidden;
-                }  
-    .block-container {
-                padding-top:1.5rem
-                } 
-    h1{
-        font-family:'Climate Crisis', sans-serif !important;
-        font-size: 3.5rem !important ;
-        line-height: 1.1 !important;
-        margin-bottom: 0rem !important   ;  
-        color: #e0e3ff       !important;    
-                } 
-    h2{
-        font-family:'Climate Crisis', sans-serif !important;
-        font-size: 2rem !important ;
-        line-height: 1.1 !important;
-        margin-bottom: 0rem !important    ;   
-        color: black !important;          
-                }   
+        <style>
+        @import url('https://fonts.googleapis.com/css2?family=Climate+Crisis:YEAR@1979&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap');
+                # #MainMenu,footer,header{
+                #     visibility:hidden
+                # }
+                .block-container{
+                    padding:1.5rem !important;}
+                h1{
+                    font-family:'Climate Crisis', sans-serif !important;
+                    font-size:3.5rem !important;
+                    line-height:1.1 !important;
+                    margin-bottom:0rem !important;
+                    color:#E0E3FF !important;
+                    }                
+                h2{
+                    font-family:'Climate Crisis', sans-serif !important;
+                    font-size:2rem !important;
+                    line-height:1.1 !important;
+                    margin-bottom:0rem !important;
+                    color:black !important;
+                    }
+                h3, h4, p{
+                font-family:'Outfit',sans-serif;
+                }
 
-    h3,h4,p{
-        font-family: 'Outfit', sans-serif; 
-                }    
+                button[kind="primary"]{
+                    border-radius:1.5rem !important;
+                    background-color:#5865F2 !important;
+                    color:white !important;
+                    padding:10px 20px !important;
+                    border:none !important;
+                    transition:transform 0.25s ease-in-out !important;
+                    }
+                button:hover{
+                    transform:scale(1.05)
+                    }
+                button[kind="secondary"]{
+                    border-radius:1.5rem !important;
+                    background-color:#EB459E !important;
+                    color:white !important;
+                    padding:10px 20px !important;
+                    border:none !important;
+                    transition:transform 0.25s ease-in-out !important;
+                button[kind="secondary"]:hover{
+                    transform:scale(1.05)
+                    }
+                button[kind="tertiary"]{
+                    border-radius:1.5rem !important;
+                    background-color:#0A0A0A !important;
+                    color:white !important;
+                    padding:10px 20px !important;
+                    border:none !important;
+                    transition:transform 0.25s ease-in-out !important;
+                    }
+                button[kind="tertiary"]:hover{
+                    transform:scale(1.05)
+                    }              
+        </style>
 
-    button[kind="primary"]{
-        border-radius: 1.5rem !important;
-        background: #5865f2 !important;
-        color: white !important;
-        padding: 10px 20px !important;
-        border:none !important;
-        transition: transform 0.25s ease-in-out !important;
-    }
-    button:hover{
-        transform:scale(1.05)}
-                
-    button[kind="secondary"]{
-        border-radius: 1.5rem !important;
-        background: #eb459e !important;
-        color: white !important;
-        padding: 10px 20px !important;
-        border:none !important;
-        transition: transform 0.25s ease-in-out !important;
-    }
-    button[kind:"secondary"]:hover{
-        transform:scale(1.05)}
-                
-    button[kind="tertiery"]{
-        border-radius: 1.5rem !important;
-        background: #0a0a0a !important;
-        color: white !important;
-        padding: 10px 20px !important;
-        border:none !important;
-        transition: transform 0.25s ease-in-out !important;
-    }
-    button[kind="tertiery"]:hover{
-        transform:scale(1.05)}
-    </style>            
-
-""", unsafe_allow_html=True)
+                """
+            ,unsafe_allow_html=True)
